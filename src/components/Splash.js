01a017ef34bdd07f7f0aa866ed16bbb769097cfa
Dhrubajyoti Das <ddas@ucdavis.edu>
2019-10-01 13:02:25 -0700

    react tilt

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Segment, Divider } from 'semantic-ui-react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Tilt from 'react-tilt';
 
 export default class Splash extends React.Component {
     render (){
@@ -12,11 +13,11 @@ export default class Splash extends React.Component {
             >
             <h1 className="splash-text">
                 Hi <span class="ec ec-wave"></span> 
-                My name is Dhrubajyoti Das  (You can call me Drew).
+                My name is Dhrubajyoti Das  (You can also call me Drew).
                 <Divider hidden />
                 I am a Full-Stack Developer who likes to build things. 
-                This website is a tool I created to showcase <span class="highlight-container"><span class="highlight"><span class="ec ec-rocket"></span> Products</span> </span>
-                I have worked on, 
+                This website is a tool I created to showcase <Tilt className="Tilt highlight-container" options={{ max : 35, perspective: 1000, scale: 1.5 }}><span className="Tilt-inner highlight"><span className="ec ec-rocket"></span> Products</span></Tilt>
+                I have built, Latest Updates below showcase what I am currently working on.
             </h1>
 
             </Segment>
